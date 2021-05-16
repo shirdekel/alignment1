4384fdc9d12cde7bd3ab22cebf35214a7ce900f1
Shir Dekel <shirdekel@yahoo.com.au>
2021-05-16 20:33:49 +1000

    Add data

---
+++ R/alignment1.R
@@ -0,0 +1,41 @@
+#' alignment1
+#' A data package for alignment1.
+#' @docType package
+#' @aliases alignment1-package
+#' @title Package Title
+#' @name alignment1
+#' @description A description of the data package
+#' @details Use \code{data(package='alignment1')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{data}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name data
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{id}{}
+#' \item{alignment}{}
+#' \item{reliability_amount}{}
+#' \item{sex}{}
+#' \item{age}{}
+#' \item{project}{}
+#' \item{allocation}{}
+#' \item{confidence}{}
+#' \item{npv_amount}{}
+#' \item{sample}{}
+#' \item{max_npv}{}
+#' \item{min_npv}{}
+#' \item{max_min_difference}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{alignment1}
+NULL
